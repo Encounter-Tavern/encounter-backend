454b4aaee6d3256adefa17cdd6230052e259d308
MasterEvarior <masterevarior@protonmail.com>
2020-09-19 21:58:10 +0200

    WIP swagger, documented all the dtos

--- src/main/java/com/encountertavern/demo/dto/ActionDto.java
+++ src/main/java/com/encountertavern/demo/dto/ActionDto.java
@@ -1,6 +1,7 @@
 package com.encountertavern.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +9,26 @@ import java.util.List;
 
 @Getter
 @Setter
+@Schema
 public class ActionDto {
 
+    @Schema(required = true,
+            description = "The name of this action",
+            example="Shortbow")
     private String name;
+
+    @Schema(required = true,
+            description = "Description of this action",
+            example="Ranged Weapon Attack: +4 to hit, range 80/320 ft., one target. Hit: 5 (1d6 + 2) piercing damage.")
     private String desc;
+
+    @Schema(required = true,
+            description = "The bonus for the attack roll",
+            example="4")
     @JsonProperty("attack_bonus")
     private int attackBonus;
+
+    @Schema(required = true,
+            description = "What damage this action causes")
     private List<DamageDto> damage;
 }
--- src/main/java/com/encountertavern/demo/dto/GenerateEncounterDto.java
+++ src/main/java/com/encountertavern/demo/dto/GenerateEncounterDto.java
@@ -1,6 +1,7 @@
 package com.encountertavern.demo.dto;
 
 import com.encountertavern.demo.enums.Difficulty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,10 +9,21 @@ import java.util.List;
 
 @Getter
 @Setter
+@Schema(description = "Can be used to quickly generate a new encounter")
 public class GenerateEncounterDto {
 
+    @Schema(required = true,
+            description = "The name of this to be generated encounter",
+            example = "Forest ambush")
     private String name;
+
+    @Schema(required = true,
+            description = "List of players that are present in this to be generated encounter")
     private List<PlayerDto> players;
+
+    @Schema(required = true,
+            description = "Difficulty level of the to be generated encounter",
+            example = "NORMAL")
     private Difficulty difficulty;
 
 }
