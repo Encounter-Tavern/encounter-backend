982024b2f91997f6d3d5c214e61de98765ea322c
MasterEvarior <masterevarior@protonmail.com>
2020-09-19 20:30:45 +0200

    Added swagger dependencies and configs

---
+++ src/main/java/com/encountertavern/demo/service/DiceRollService.java
@@ -0,0 +1,15 @@
+package com.encountertavern.demo.service;
+
+import org.hibernate.cfg.NotYetImplementedException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiceRollService {
+    public long rollDice(String dice){
+        throw new NotYetImplementedException();
+    }
+
+    public long rollDice(String dice, long baseValue){
+        throw new NotYetImplementedException();
+    }
+}
